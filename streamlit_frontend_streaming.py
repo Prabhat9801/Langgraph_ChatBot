74e85ec45c5c00bb2b31ed5fb8fdb93c5b82d951
Prabhat9801 <prabhatkumarsictc7070@gmail.com>
2025-09-25 16:47:54 +0530

    Added the feature of resume the conversation using the concept of threading

--- streamlit_frontend_streaming.py
+++ streamlit_frontend_streaming.py
@@ -2,46 +2,38 @@ import streamlit as st
 from Langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
 
-CONFIG = {"configurable": {"thread_id": "thread-1"}}
+# st.session_state -> dict -> 
+CONFIG = {'configurable': {'thread_id': 'thread-1'}}
 
-if "message_history" not in st.session_state:
-    st.session_state["message_history"] = []
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
 
-# Display previous conversation history
-for message in st.session_state["message_history"]:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])  # 👈 render as Markdown
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
 
-# User input
-user_input = st.chat_input("Type here")
+#{'role': 'user', 'content': 'Hi'}
+#{'role': 'assistant', 'content': 'Hi=ello'}
+
+user_input = st.chat_input('Type here')
 
 if user_input:
-    # Save user message
-    st.session_state["message_history"].append(
-        {"role": "user", "content": user_input}
-    )
-    with st.chat_message("user"):
-        st.markdown(user_input)
 
-    # Assistant streaming response
-    with st.chat_message("assistant"):
-        placeholder = st.empty()
-        full_response = ""
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
 
-        # Stream response chunks
-        for message_chunk, metadata in chatbot.stream(
-            {"messages": [HumanMessage(content=user_input)]},
-            config=CONFIG,
-            stream_mode="messages"
-        ):
-            if message_chunk.content:
-                full_response += message_chunk.content
-                placeholder.markdown(full_response + "▌")  # 👈 Markdown + typing cursor
+    # first add the message to message_history
+    with st.chat_message('assistant'):
 
-        # Finalize response
-        placeholder.markdown(full_response)
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in chatbot.stream(
+                {'messages': [HumanMessage(content=user_input)]},
+                config= {'configurable': {'thread_id': 'thread-1'}},
+                stream_mode= 'messages'
+            )
+        )
 
-    # Save assistant reply
-    st.session_state["message_history"].append(
-        {"role": "assistant", "content": full_response}
-    )
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
